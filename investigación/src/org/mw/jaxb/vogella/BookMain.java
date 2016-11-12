c5f26e9e10f12759fdc6929ef49fde35d6c160e8
moonwave <moonwave.systems@gmail.com>
2016-11-12 15:40:33 -0700

    write to and read from xml string

--- investigación/src/org/mw/jaxb/vogella/BookMain.java
+++ investigación/src/org/mw/jaxb/vogella/BookMain.java
@@ -3,8 +3,11 @@ package org.mw.jaxb.vogella;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.ArrayList;
 
+import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
@@ -49,19 +52,43 @@ public class BookMain {
         m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
         // Write to System.out
+        System.out.println("===== m.marshal(bookstore, System.out) =====");
         m.marshal(bookstore, System.out);
+
+        // http://stackoverflow.com/questions/26959343/convert-java-object-to-xml-string
+        StringWriter sw = new StringWriter();
+        m.marshal(bookstore, sw);
+        String xmlString = sw.toString();
+        System.out.println("\n===== from xmlString =====");
+        System.out.println(xmlString);
 
         // Write to File
         m.marshal(bookstore, new File(BOOKSTORE_XML));
 
+        // =====================================================================
         // get variables from our xml file, created before
         System.out.println();
         System.out.println("Output from our XML File: ");
         Unmarshaller um = context.createUnmarshaller();
         Bookstore bookstore2 = (Bookstore) um.unmarshal(new FileReader(BOOKSTORE_XML));
+
+        // http://stackoverflow.com/questions/5458833/use-jaxb-to-create-object-from-xml-string
+        Bookstore bookstore3 = (Bookstore) um.unmarshal(new StringReader(xmlString));
+        Bookstore bookstore4 = JAXB.unmarshal(new StringReader(xmlString), Bookstore.class);
+
+        System.out.println("\nbookstore2: ");
         ArrayList<Book> list = bookstore2.getBooksList();
         for (Book book : list) {
             System.out.println("Book: " + book.getName() + " from " + book.getAuthor());
         }
-}
+        System.out.println("\nbookstore3: ");
+        for (Book book : bookstore3.getBooksList()) {
+            System.out.println("Book: " + book.getName() + " from " + book.getAuthor());
+        }
+        System.out.println("\nbookstore4: ");
+        for (Book book : bookstore4.getBooksList()) {
+            System.out.println("Book: " + book.getName() + " from " + book.getAuthor());
+        }
+        
+    }
 }
