f8d9a9c5645db24811878d3cad90ad31b47ec411
moonwave <moonwave.systems@gmail.com>
2016-11-23 19:49:35 -0700

    removed throws exceptions

--- investigación/src/org/mw/jaxb/JaxbUtil.java
+++ investigación/src/org/mw/jaxb/JaxbUtil.java
@@ -2,13 +2,14 @@ package org.mw.jaxb;
 
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
 
@@ -17,6 +18,8 @@ import javax.xml.stream.XMLStreamReader;
  *
  */
 public class JaxbUtil<T> {
+
+    private static final Logger LOGGER = Logger.getLogger(JaxbUtil.class.getName());
 
     /**
      * Marshal: Convert a Java object to XML string
@@ -27,14 +30,21 @@ public class JaxbUtil<T> {
      *   String xmlString = jaxbUtil.marshal(emp);
      * @param t
      * @return
-     * @throws JAXBException
      */
-    public String marshal(T t) throws JAXBException {
-        final Marshaller m = JAXBContext.newInstance(t.getClass()).createMarshaller();
-        m.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
-        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    public String marshal(T t) {
+        if (t == null) {
+            return null;
+        }
         final StringWriter w = new StringWriter();
-        m.marshal(t, w);
+        try {
+            final Marshaller m = JAXBContext.newInstance(t.getClass()).createMarshaller();
+            m.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8");
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            m.marshal(t, w);
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, null, e);
+        }
         return w.toString();
     }
 
@@ -52,28 +62,46 @@ public class JaxbUtil<T> {
      */
     @SuppressWarnings("unchecked")
     public T unmarshal(Class<T> clazz, final String xml) throws JAXBException {
-        return (T) JAXBContext.newInstance(clazz)
-                   .createUnmarshaller()
-                   .unmarshal(new StringReader(xml));
+        try {
+            return (T) JAXBContext.newInstance(clazz)
+                       .createUnmarshaller()
+                       .unmarshal(new StringReader(xml));
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, null, e);
+        }
+        return null;
     }
 
-	public static <T> T unmarshal(final String xml, Class<T> t) throws JAXBException, XMLStreamException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(t);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        XMLInputFactory xmlif = XMLInputFactory.newFactory();
-        xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
-        xmlif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        XMLStreamReader xmlsr = xmlif.createXMLStreamReader(new StringReader(xml));
-        return t.cast(unmarshaller.unmarshal(xmlsr));
+    public static <T> T unmarshal(final String xml, Class<T> t) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(t);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            XMLInputFactory xmlif = XMLInputFactory.newFactory();
+            xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+            xmlif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+            XMLStreamReader xmlsr = xmlif.createXMLStreamReader(new StringReader(xml));
+            return t.cast(unmarshaller.unmarshal(xmlsr));
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, null, e);
+        }
+        return null;
     }
 
-	public static <T> T unmarshal(Class<T> t, Class subClazz, final String xml) throws JAXBException, XMLStreamException {
-        JAXBContext jaxbContext = JAXBContext.newInstance(t, subClazz);
-        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        XMLInputFactory xmlif = XMLInputFactory.newFactory();
-        xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
-        xmlif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        XMLStreamReader xmlsr = xmlif.createXMLStreamReader(new StringReader(xml));
-        return t.cast(unmarshaller.unmarshal(xmlsr));
+    public static <T> T unmarshal(Class<T> t, Class subClazz, final String xml) {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(t, subClazz);
+            Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+            XMLInputFactory xmlif = XMLInputFactory.newFactory();
+            xmlif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+            xmlif.setProperty(XMLInputFactory.SUPPORT_DTD, false);
+            XMLStreamReader xmlsr = xmlif.createXMLStreamReader(new StringReader(xml));
+            return t.cast(unmarshaller.unmarshal(xmlsr));
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, null, e);
+        }
+        return null;
     }
 }
