a5fbfbb5db4e2658d36d8aa50581404b0934db0a
moonwave <moonwave.systems@gmail.com>
2017-01-17 15:55:04 -0700

    add mongo java driver

---
+++ investigación/src/org/mw/mongodb/MongoDBJDBC.java
@@ -0,0 +1,41 @@
+package org.mw.mongodb;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoException;
+import com.mongodb.WriteConcern;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.DBCursor;
+
+import com.mongodb.ServerAddress;
+import java.util.Arrays;
+
+/**
+ * 
+ * https://www.tutorialspoint.com/mongodb/mongodb_java.htm
+ * https://oss.sonatype.org/content/repositories/releases/org/mongodb/mongo-java-driver/
+ *   mongo-java-driver-3.2.2.jar or higher
+ */
+public class MongoDBJDBC {
+
+   public static void main( String args[] ) {
+
+      try{
+        String myUserName, myPassword;
+         // To connect to mongodb server
+         MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+            
+         // Now connect to your databases
+         DB db = mongoClient.getDB( "test" );
+         System.out.println("Connect to database successfully");
+//         boolean auth = db.authenticate(myUserName, myPassword);
+//         System.out.println("Authentication: "+auth);
+
+      }catch(Exception e){
+         System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+      }
+   }
+}
