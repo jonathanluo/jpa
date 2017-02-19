c14d68e99758b22c3a30f1aa15f19c9bbdcf504e
moonwave <moonwave.systems@gmail.com>
2017-02-18 22:16:00 -0700

    restrictions on generics

--- investigación/src/org/mw/generics/TypeErasure.java
+++ investigación/src/org/mw/generics/TypeErasure.java
@@ -1,10 +1,20 @@
 package org.mw.generics;
 
 /**
+ * https://docs.oracle.com/javase/tutorial/java/generics/erasure.html
  * https://docs.oracle.com/javase/tutorial/java/generics/bridgeMethods.html
+
+   Type Erasure is the process where the compiler removes information related to type parameters and type arguments.
+
+   - Replace all type parameters in generic types with their bounds or Object if the type parameters are unbounded. The 
+     produced bytecode, therefore, contains only ordinary classes, interfaces, and methods.
+   - Insert type casts if necessary to preserve type safety.
+   - Generate bridge methods to preserve polymorphism in extended generic types.
+
+   Type erasure ensures that no new classes are created for parameterized types; consequently, generics incur no runtime
+   overhead.
  */
 public class TypeErasure {
-
 
     public static void main(String[] argv) {
         try {
