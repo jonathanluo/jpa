b5d72d84c5e888d3f8e48269ee2b1cd87fd115c5
moonwave <moonwave.systems@gmail.com>
2017-02-20 15:47:09 -0700

    java 7

---
+++ investigación/src/org/mw/java7/StringsInSwitch.java
@@ -0,0 +1,40 @@
+package org.mw.java7;
+
+/**
+ * http://docs.oracle.com/javase/7/docs/technotes/guides/language/strings-switch.html
+ */
+public class StringsInSwitch {
+
+    public String getTypeOfDayWithSwitchStatement(String dayOfWeekArg) {
+         System.out.println("dayOfWeekArg: " + dayOfWeekArg);
+         String typeOfDay;
+         switch (dayOfWeekArg) {
+             case "Monday":
+                 typeOfDay = "Start of work week";
+                 break;
+             case "Tuesday":
+             case "Wednesday":
+             case "Thursday":
+                 typeOfDay = "Midweek";
+                 break;
+             case "Friday":
+                 typeOfDay = "End of work week";
+                 break;
+             case "Saturday":
+             case "Sunday":
+                 typeOfDay = "Weekend";
+                 break;
+             default:
+                 throw new IllegalArgumentException("Invalid day of the week: " + dayOfWeekArg);
+         }
+         return typeOfDay;
+    }
+
+    public static void main(String[] argv) {
+        StringsInSwitch test= new StringsInSwitch();
+        System.out.println(test.getTypeOfDayWithSwitchStatement("Monday"));
+        System.out.println(test.getTypeOfDayWithSwitchStatement("Sunday"));
+        System.out.println(test.getTypeOfDayWithSwitchStatement("Tuesday"));
+        System.out.println(test.getTypeOfDayWithSwitchStatement("Friday"));
+    }
+}
---
+++ investigación/src/org/mw/java7/UnderscoresInNumericLiterals.java
@@ -0,0 +1,27 @@
+package org.mw.java7;
+
+/**
+ * http://docs.oracle.com/javase/7/docs/technotes/guides/language/underscores-literals.html
+ */
+public class UnderscoresInNumericLiterals {
+
+    public static void main(String[] argv) {
+        long creditCardNumber = 1234_5678_9012_3456L;
+        long socialSecurityNumber = 999_99_9999L;
+        float pi = 3.14_15F;
+        float pi2 = 3.1_4_159F;
+        long hexBytes = 0xFF_EC_DE_5E;
+        long hexWords = 0xCAFE_BABE;
+        long maxLong = 0x7fff_ffff_ffff_ffffL;
+        byte nybbles = 0b0010_0101;
+        long bytes = 0b11010010_01101001_10010100_10010010;
+
+        long ipaddress = 101_258_274_670L;
+
+        System.out.println("creditCardNumber: " + creditCardNumber);
+        System.out.println("socialSecurityNumber: " + socialSecurityNumber);
+        System.out.println("pi: " + pi);
+        System.out.println("pi2: " + pi2);
+        System.out.println("ipaddress: " + ipaddress);
+    }
+}
