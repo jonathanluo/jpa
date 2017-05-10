2bd57230ec403c2e7e5e8e1a37d8175a57923ff3
moonwave <moonwave.systems@gmail.com>
2017-05-09 19:04:32 -0700

    comment

---
+++ investigación/src/org/mw/nosql/mongodb/Mongo.java
@@ -0,0 +1,19 @@
+package org.mw.nosql.mongodb;
+
+/**
+ * Import Example Dataset, https://docs.mongodb.com/getting-started/shell/import-data/
+ * wget https://raw.githubusercontent.com/mongodb/docs-assets/primer-dataset/primer-dataset.json
+ * mongoimport --db test --collection restaurants --drop --file ~/Downloads/primer-dataset.json
+ *
+ * https://docs.mongodb.com/getting-started/shell/client/
+ * start   : mymongo.sh
+ * shutdown: mymongo-shutdown.sh
+ * console : mongo
+ *
+ */
+public class Mongo {
+
+   public static void main( String args[] ) {
+
+   }
+}
