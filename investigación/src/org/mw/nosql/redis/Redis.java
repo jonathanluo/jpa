58b15b26ff0401e2e6dca11ba20ef52c2891c850
moonwave <moonwave.systems@gmail.com>
2017-05-09 21:34:43 -0700

    redis startup comment

--- investigación/src/org/mw/nosql/redis/Redis.java
+++ investigación/src/org/mw/nosql/redis/Redis.java
@@ -5,6 +5,13 @@ import redis.clients.jedis.Jedis;
  * https://www.tutorialspoint.com/redis/redis_java.htm
  * https://mvnrepository.com/artifact/redis.clients/jedis/2.9.0
  *
+ * Build redis
+ * cd /j01/srv/redis/
+ * make
+ * 
+ * Start redis
+ * /j01/srv/redis/src/redis-server
+ *
  */
 public class Redis { 
    public static void main(String[] args) { 
